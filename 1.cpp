77cea4554081fd7730337d1dbbfaa9bf58e41263
ni511 <103868519+ni511@users.noreply.github.com>
2022-05-30 23:56:47 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,5 @@
 
-//Write a program in C++ to find the first 10 natural numbers.#include<iostream>
+//Write a program in C++ to find the first 10 natural numbers.
 #include<iostream>
 using namespace std;
 int  main()
